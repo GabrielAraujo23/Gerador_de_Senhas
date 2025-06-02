0c8bb5599cc4246ffd6e4ed05cb7de0ed403ef04
Gabriel <gabrielaraujolima23@hotmail.com>
2025-06-02 20:37:53 -0300

    Gerador de Senhas

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
+
 // Seleção de Elementos
 const generatePasswordButton = document.querySelector("#generate-password");
 const generatedPasswordElement = document.querySelector("#generated-password");
 
 // Novas funcionalidades
-const openCloseGeneratorButton = document.querySelector(
-  "#open-generate-password"
-);
 const generatePasswordContainer = document.querySelector("#generate-options");
 const lengthInput = document.querySelector("#length");
 const lettersInput = document.querySelector("#letters");
 const numbersInput = document.querySelector("#numbers");
 const symbolsInput = document.querySelector("#symbols");
 const copyPasswordButton = document.querySelector("#copy-password");
+const nivelValidacao = document.querySelector("#nivel-validacao");
+
 
 // Funções
 const getLetterLowerCase = () => {
@@ -87,9 +87,6 @@ generatePasswordButton.addEventListener("click", () => {
   );
 });
 
-openCloseGeneratorButton.addEventListener("click", () => {
-  generatePasswordContainer.classList.toggle("hide");
-});
 
 copyPasswordButton.addEventListener("click", (e) => {
   e.preventDefault();
@@ -104,3 +101,81 @@ copyPasswordButton.addEventListener("click", (e) => {
     }, 1000);
   });
 });
+
+
+function validarSenha(event) {
+  const form = document.getElementById('senha');
+  const erro = document.getElementById("mensagem-erro");
+  const senha = form.value;
+  erro.innerText = ""; // limpa erro anterior
+
+  if (senha.length < 7) {
+    erro.innerText = "Insira uma senha válida de no mínimo 7 caracteres.";
+    return;
+  }
+  event.preventDefault();
+  console.log(form.value[0])
+  var nivel = 0
+  if (containsUppercase(form.value)) {
+      console.log("tem maiscula");
+      nivel=nivel+1
+  }
+  if(containsNumber(form.value)){
+      console.log("tem numero");
+      nivel=nivel+1
+  }
+  if(containsSpecial(form.value)){
+      console.log("tem especial");
+      nivel=nivel+1
+  }
+  nivelValidacao.style.display = "block";
+  nivelValidacao.querySelector("h4").innerText = "O nível da sua senha é: " +nivel;
+  
+}
+
+
+function containsUppercase(str){
+  return /[A-Z]/.test(str);
+}
+
+function containsNumber(str){
+  return /\d/.test(str);
+}
+
+function containsSpecial(str){
+  return /[\W_]/.test(str);
+}
+
+
+function showValidador(){
+  var validador = document.getElementById("validador")
+  var btnValidador = document.getElementById("btnValidador")
+  var gerador = document.getElementById("gerador")
+  var btnGerador = document.getElementById("btnGerador")
+  
+  
+   gerador.style.display = "none";
+   validador.style.display = "block";
+   btnValidador.disabled = true
+   btnGerador.disabled = false
+   
+   document.getElementById("generated-password").style.display = "none";
+
+}
+
+function showGerador(){
+  var gerador = document.getElementById("gerador")
+  var btnGerador = document.getElementById("btnGerador")
+  var validador = document.getElementById("validador")
+  var btnValidador = document.getElementById("btnValidador")
+
+   validador.style.display = "none";
+   gerador.style.display = "block";
+   btnGerador.disabled = true
+   btnValidador.disabled = false
+   
+
+   document.getElementById("nivel-validacao").style.display = "none";
+}
+
+  
